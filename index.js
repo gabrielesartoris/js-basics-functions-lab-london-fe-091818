85647ba8cb269e4f36e5b9af81250d7671bbf476
Gabriele Sartoris <sartoris.gabriele@gmail.com>
2018-11-12 01:29:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block)  {
+  let hq = 42;
+  if (block < hq) {
+    return hq - block;
+  }
+  return block - hq;
+}
+
+function distanceFromHqInFeet(block) {
+  let blocks= distanceFromHqInBlocks(block);
+  return blocks * 264;
+}
+
+function distanceTravelledInFeet(startingblock, endingblock) {
+  return (startingblock - endingblock) * 264
+  
+  if (distanceFromHqInFeet > 0 ) {
+  return (startingblock - endingblock) * 264
+  }
+}
+
+function calculatesFarePrice() {
+  
+}
